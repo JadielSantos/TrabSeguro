8cc670a4de360d1cec32721fe1050a0402238ba8
Jadiel Santos <jadielsantosjds@gmail.com>
2019-09-05 01:30:19 -0300

    terminar sql

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using testweb.Model;
 
 namespace testweb.Pages
 {
@@ -16,8 +18,22 @@ namespace testweb.Pages
 
 		[Route("api/login")]
 		[HttpGet]
-		protected void Login(object sender, EventArgs e)
+		protected User Login()
 		{
+			User user = new User();
+			string query = string.Empty;
+			string connetionString = @"Data Source=JADIEL\LOCALSQL;Initial Catalog=Geral;User ID=adm;Password=adm@123";
+
+			using (SqlConnection cnn = new SqlConnection(connetionString))
+			{
+				cnn.Open();
+
+				query += "SELECT * FROM [Geral].[dbo].[User] WHERE Nome = @Nome AND Senha = @Senha";
+
+				cnn.Close();
+			}
+
+			return user;
 		}
 	}
 }
